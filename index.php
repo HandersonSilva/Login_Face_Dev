8680f079db1ccddcff6bfc36f4e26e5e05408cbd
Handerson Silva <handersonsylva@gmail.com>
2017-10-18 20:31:39 -0300

    Revendo o campo public_profile

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                 // for FB.getLoginStatus().
                 if (response.status === 'connected') {
                 // Logged into your app and Facebook.
-                FB.api('/me', {fields:'public_profile,email'} , function(response) {
+                FB.api('/me', {fields:'email'} , function(response) {
                         /*Response tem tudo que você solicitou, inclusive o access_token.*/
                         console.log(response);
                         console.log('Successful login for: ' + response.name+" "+response.email);
