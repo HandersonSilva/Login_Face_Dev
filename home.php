0c92ad5aa1488eff0e409c9058d1073ddf48518c
Handerson Silva <handersonsylva@gmail.com>
2017-10-20 22:18:18 -0300

    consertando loop session 3

--- home.php
+++ home.php
@@ -10,14 +10,14 @@ session_start();
     </head>
     <body>
 
-    <?php if(  isset($_SESSION['login_face'])&& $_SESSION['login_face']== 'connected'){ ?>
+    <?php if(  isset($_SESSION['login_face'])&& $_SESSION['login_face']== 'connected'){ echo $_SESSION['login_face'];?>
         <h1>LOGADO COM SUCESSO!!!</h1>
 
         <div id="status">
         </div>
 
-    <?php } else{?>
-        <script>  window.location = "http://handersonsilva.com/Login_Face_Dev/"; </script>
+    <?php } else{  echo $_SESSION['login_face'];?>
+        <!--<script>  window.location = "http://handersonsilva.com/Login_Face_Dev/"; </script>-->
     <?php }?>
        
         <input type="button" value="Logout" onclick="FB.logout();">
