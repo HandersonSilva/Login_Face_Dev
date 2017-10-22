d95c6d0dbfc4191fe94e8e28b5e3724f64c0371d
Handerson Silva <handersonsylva@gmail.com>
2017-10-22 14:30:59 -0300

    Pegando o retorno do logout

--- sucesso.php
+++ sucesso.php
@@ -16,7 +16,7 @@
         <body>
        
   
-            <fb:login-button autologoutlink="true" onlogin="getLoginStatus(); ">
+            <fb:login-button autologoutlink="true" onlogin="checkLoginState();">
             </fb:login-button>
             
     
